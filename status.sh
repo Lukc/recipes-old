e0f2efb47c16916a44fb6a0817e5a2b45606cf75
Lukc <lukc@upyum.com>
2011-12-06 18:41:11 +0100

    status.sh updates.

--- status.sh
+++ status.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env zsh
 
+# Things to avoid having too much parsing errors. o/
+PKGMK_ROOT=.
+
+export PATH="$PATH:/usr/libexec/pkg++/"
+alias use=true
+alias use_with=true
+alias use_enable=true
+. /etc/pkg++/*/uses
+include() {
+	for p in . ../includes /usr/share/pkg++/includes; do
+		[[ -e $p/$1 ]] && . $p/$1 && return 0
+	done
+}
+
+echo "=== {{ pkg++ standards compliance }} ==="
+
 for packager in Youbi piernov Kooda Lukc; do
 	echo "%% {{ $packager }} %%"
 	: > log.$packager
@@ -22,4 +38,49 @@ for packager in Youbi piernov Kooda Lukc; do
 	echo -n "Correct packages: "; grep "Everything is in perfect order." log.$packager | wc -l
 done
 
+echo "=== {{ Orphan packages }} ==="
+
+for i in *; do
+	if [[ "$i" = includes || "$i" =~ log\..* || "$i" = status.sh ]]; then
+		continue
+	fi
+	(
+		cd "$i"
+		local pkgfile="$(ls | grep Pkgfile | (tail -n 1 || cat))"
+		. ./$pkgfile
+		if [[ -z "$maintainer" || -z "$packager" ]]; then
+			echo "-- [[ $i ]]"
+		fi
+		if [[ -z "$maintainer" ]]; then
+			echo "  > No maintainer."
+		fi
+		if [[ -z "$packager" ]]; then
+			echo "  > No packager."
+		fi
+	)
+done
+
+echo "=== {{ useflags }} ==="
+
+for i in *; do
+	if [[ "$i" = includes || "$i" =~ log\..* || "$i" = status.sh ]]; then
+		continue
+	fi
+	(
+		cd "$i"
+		local pkgfile="$(ls | grep Pkgfile | (tail -n 1 || cat))"
+		. ./$pkgfile
+		if [[ -n "${iuse[@]}" ]]; then
+			echo "-- [[ $i ]]"
+			echo "  > iuse declared"
+			for flag in ${iuse[@]}; do
+				if [[ -n "$(eval echo "\$use_$flag")" ]]; then
+					echo "    > $flag"
+				else
+					echo "    > $flag [not defined]"
+				fi
+			done
+		fi
+	)
+done
 
