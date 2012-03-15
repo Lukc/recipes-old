0b0ba31654cdb9c89372351d92af2c4af6b6c675
Lukc <lukc@upyum.com>
2012-03-15 09:12:14 +0100

    status.sh update.

--- status.sh
+++ status.sh
@@ -19,7 +19,7 @@ include() {
 }
 
 is_a_port() {
-	if [[ "$1" = includes || "$1" =~ log\..* || "$1" =~ .*\.sh || "$1" = tmp\..* || ! -d "$1" ]]; then
+	if [[ "$1" = includes || "$1" =~ log\..* || "$1" =~ .*\.sh || "$1" =~ tmp\..* || ! -d "$1" ]]; then
 		return 1
 	fi
 }
@@ -46,6 +46,7 @@ subtitle() {
 	if [[ "$OUTPUT" = html ]]; then
 		echo "<h3>$(html_escape "$@")</h3>"
 	else
+		echo # The empty line never hurts.
 		echo "-= {{ $@ }} =-"
 	fi
 }
@@ -111,85 +112,137 @@ colors
 
 . ./tmp.pkgxx.config
 
-title "pkg++ standards compliance"
-
-for packager in Youbi Kooda Lukc; do
-	subtitle "$packager"
-	: > log.$packager
-	for i in *; do
-		(
-			if ! is_a_port "$i"; then
-				continue
-			fi
-			cd "$i"
-			if egrep -q "$packager" Pkgfile*; then
-				{
-					subsubtitle "$i"
-					pkg++ -cf ../tmp.pkgxx.config -cp 2> log.pkgxx-error
-				} | tee -a ../log.$packager
-			fi
-			if [[ -e log.pkgxx-error ]]; then
-				if [[ "$OUTPUT" = html ]]; then
-					sed -e "s/^/<div id=\"zsh-error\">/;s/$(echo -n -e "\033")/\\e/g;s/$/<\/div>/" log.pkgxx-error
-				else
-					cat log.pkgxx-error
+function test_general {
+	title "pkg++ standards compliance"
+	
+	if [[ -z "${users[@]}" ]]; then
+		users=(Youbi Kooda Lukc Pingax)
+	fi
+	
+	for packager in ${users[@]}; do
+		subtitle "$packager"
+		: > log.$packager
+		for i in *; do
+			(
+				if ! is_a_port "$i"; then
+					continue
 				fi
-				rm log.pkgxx-error
+				cd "$i"
+				if egrep -q "$packager" Pkgfile*; then
+					{
+						subsubtitle "$i"
+						pkg++ -cf ../tmp.pkgxx.config -cp 2> log.pkgxx-error
+					} | tee -a ../log.$packager
+				fi
+				if [[ -e log.pkgxx-error ]]; then
+					if [[ "$OUTPUT" = html ]]; then
+						sed -e "s/^/<div id=\"zsh-error\">/;s/$(echo -n -e "\033")/\\e/g;s/$/<\/div>/" log.pkgxx-error
+					else
+						cat log.pkgxx-error
+					fi
+					rm log.pkgxx-error
+				fi
+			)
+		done
+		subsubtitle "Summary:"
+		info "Errors:           $(grep "$ERROR" log.$packager | wc -l)"
+		info "Warnings:         $(grep "$WARNING" log.$packager | wc -l)"
+		info "Correct packages: $(grep "Everything is in perfect order." log.$packager | wc -l)"
+	done
+}
+
+
+function test_orphans {
+	subtitle "Orphan packages"
+
+	for i in *; do
+		if ! is_a_port "$i"; then
+			continue
+		fi
+		(
+			cd "$i"
+			local pkgfile="$(ls | grep Pkgfile | (tail -n 1 || cat))"
+			. ./$pkgfile
+			if [[ -z "$maintainer" || -z "$packager" ]]; then
+				subsubtitle "$i"
+			fi
+			if [[ -z "$maintainer" ]]; then
+				info "No maintainer."
+			fi
+			if [[ -z "$packager" ]]; then
+				info "No packager."
 			fi
 		)
 	done
-	subsubtitle "Summary:"
-	info "Errors:           $(grep "$ERROR" log.$packager | wc -l)"
-	info "Warnings:         $(grep "$WARNING" log.$packager | wc -l)"
-	info "Correct packages: $(grep "Everything is in perfect order." log.$packager | wc -l)"
-done
+}
 
-subtitle "Orphan packages"
+function test_useflags {
+	subtitle "useflags"
 
-for i in *; do
-	if ! is_a_port "$i"; then
-		continue
-	fi
-	(
-		cd "$i"
-		local pkgfile="$(ls | grep Pkgfile | (tail -n 1 || cat))"
-		. ./$pkgfile
-		if [[ -z "$maintainer" || -z "$packager" ]]; then
-			subsubtitle "$i"
+	for DIR in *; do
+		if ! is_a_port "$DIR"; then
+			continue
 		fi
-		if [[ -z "$maintainer" ]]; then
-			info "No maintainer."
-		fi
-		if [[ -z "$packager" ]]; then
-			info "No packager."
-		fi
-	)
-done
-
-subtitle "useflags"
-
-for DIR in *; do
-	if ! is_a_port "$DIR"; then
-		continue
-	fi
-	(
-		cd "$DIR"
-		local pkgfile="$(ls | grep Pkgfile | (tail -n 1 || cat))"
-		. ./$pkgfile
-		if [[ -n "${iuse[@]}" ]]; then
-			subsubtitle "$DIR"
-			for flag in ${iuse[@]}; do
-				if [[ -n "$(eval echo "\${use_$flag[@]}")" ]]; then
-					if [[ -z "$(eval echo "\${use_${flag}[1]}")" || -z "$(eval echo "\${use_${flag}[2]}")" ]]; then
-						warning "$flag is only partially defined."
+		(
+			cd "$DIR"
+			local pkgfile="$(ls | grep Pkgfile | (tail -n 1 || cat))"
+			. ./$pkgfile
+			if [[ -n "${iuse[@]}" ]]; then
+				subsubtitle "$DIR"
+				for flag in ${iuse[@]}; do
+					if [[ -n "$(eval echo "\${use_$flag[@]}")" ]]; then
+						if [[ -z "$(eval echo "\${use_${flag}[1]}")" || -z "$(eval echo "\${use_${flag}[2]}")" ]]; then
+							warning "$flag is only partially defined."
+						else
+							info "$flag"
+						fi
 					else
-						info "$flag"
+						error "$flag is not defined."
 					fi
-				else
-					error "$flag is not defined."
-				fi
-			done
+				done
+			fi
+		)
+	done
+}
+
+function test_lastvers {
+	subtitle "bad lastvers"
+
+	for DIR in *; do
+		if ! is_a_port "$DIR"; then
+			continue
 		fi
-	)
+		(
+			cd "$DIR"
+			local pkgfile="$(ls | grep Pkgfile | (tail -n 1 || cat))"
+			. ./$pkgfile
+			subsubtitle "$DIR"
+			if [[ -n "$lastver" ]]; then
+				if [[ "$lastver" =~ "lynx -dump" ]]; then
+					error "lynx -dump is used."
+				fi
+			else
+				error "No \$lastver."
+			fi
+		)
+	done
+}
+
+
+while [[ -n "$@" ]]; do
+	case "$1" in
+		*)
+			tests=(${tests[@]} $1)
+		;;
+	esac
+	shift 1
+done
+
+if [[ -z "${tests[@]}" ]]; then
+	tests=(general orphans useflags lastvers)
+fi
+
+for test in ${tests[@]}; do
+	test_$test
 done
 
